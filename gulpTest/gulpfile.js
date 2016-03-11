1b2d0fdf24891bb3a93504534ed96ee53af6935a
weiyao86 <weiyaoisnull@gmail.com>
2016-03-11 18:31:32 +0800

    test **/test

---
+++ gulpTest/gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
+var minifyhtml = require('gulp-minify-html');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+
+var watchCss = gulp.watch('./styles/*.css');
+//build sass
+gulp.task("sass", function() {
+	//.pipe(sass({outputStyle: 'extend'})compressed
+	gulp.src('./*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./styles'))
+		.pipe(livereload());
+});
+
+//压缩js并重命名
+gulp.task("uglify", function() {
+	return gulp.src('./js/*.js')
+		.pipe(uglify())
+		.pipe(rename('custom.min.js'))
+		.pipe(gulp.dest('./release/js'));
+});
+
+//合并压缩css
+gulp.task("minify-css", ["sass"], function() {
+	return gulp.src('./styles/*.css')
+		.pipe(concat('all.css'))
+		.pipe(minifycss())
+		.pipe(gulp.dest('./release/styles'));
+});
+
+//合并压缩html
+gulp.task("minify-html", function() {
+	return gulp.src('./view/*.html')
+		.pipe(minifyhtml())
+		.pipe(gulp.dest('./view'));
+});
+
+//只重新编译被更改过的文件
+gulp.task('rebuild', function() {
+	gulp.src('./*.scss')
+		.pipe(watch('./*.scss'))
+		.pipe(sass())
+		.pipe(gulp.dest('./styles'));
+});
+
+// watchCss.on('change', function(event) {
+// 	console.log('file ' + event.path + ' was' + event.type + ' ,runing tasks...');
+// });
+
+// gulp.task("sass:watch", function() {
+// 	//listener sass change
+// 	gulp.watch('./*.scss', ['sass']);
+// });
+
+//监听改动后实时刷新
+gulp.task("watch", function(cb) {
+	// 该插件最好配合谷歌浏览器来使用，且要安装livereload chrome extension扩展插件
+	livereload.listen();
+	//scss改动时编译css并刷新页面
+	watch('./*.scss', function() {
+		gulp.start('sass');
+	});
+	//html改动时刷新页面
+	watch('./view/*.html', function() {
+		gulp.src('./view/*.html')
+			.pipe(livereload());
+	});
+});
+
+
+
+gulp.task('default', ["minify-html", "minify-css", "uglify"]);
