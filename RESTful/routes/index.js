386b4ce268aa537e4da21fbe0641d9153ec5f851
weiyao86 <weiyaoisnull@gmail.com>
2016-05-05 18:53:43 +0800

    生成菜单

--- RESTful/routes/index.js
+++ RESTful/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 var user = require('../lib/user.json');
 
 /* GET home page. */
@@ -50,15 +52,41 @@ router.get('/:id', function(req, res, next) {
 			}
 		});
 
-		res.render("menu", {
-			title: "show menu",
-			users: u,
-			menu: menus
+		var url = "http://localhost/WebTest/menu.html",
+			localUrl = "D:/Work-git/WebTest/menu.html";
+		request(url, {
+			timeout: 30 * 1000
+		}, function(err, rqres, body) {
+			if (err && err.code == "ETIMEDOUT") {
+				console.log('你报错了ETIMEDOUT...' + err);
+			} else if (!err && rqres.statusCode == 200) {
+				var $ = cheerio.load(body, {
+					decodeEntities: false //可以关闭这个转换实体编码的功能 (不然中文乱码)
+				});
+				var $menu = $("#menu");
+				var list = [];
+
+				menus.forEach(function(val) {
+					if (val.indexOf('js常用函数') == 0) console.log('<li><a href=' + val + '>' + val + '</a></li>');
+
+
+					list.push('<li><a href=' + val + '>' + val + '</a></li>');
+				});
+				$menu.empty().append(list.join(''));
+
+				fs.writeFile(localUrl, $.html(), function(err1) {
+					if (err1) console.log("writeFile-error:" + err1);
+					console.log("已替换本地文件--" + localUrl);
+					res.render("menu", {
+						title: "更新webdemo中菜单",
+						users: u,
+						menu: menus
+					});
+				});
+			}
 		});
+
 	});
-
-
-
 });
 
 
